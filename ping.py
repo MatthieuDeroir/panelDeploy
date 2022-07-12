6452e04845c82ab6d3175db77f4f0b2d934996b2
MatthieuDeroir <matthieu.deroir@epsi.fr>
2022-07-12 10:59:51 +0200

    config

--- ping.py
+++ ping.py
@@ -1,6 +1,12 @@
+import re
 import subprocess
+
 
 def ping(host):
     command = ['ping', '-c', '1', '-t', '1', host]
-    return subprocess.call(command)
+    ping = subprocess.Popen(command, stdout=subprocess.PIPE)
+    timestr = re.compile("time=[0-9]+ms").findall(str(ping.communicate()[0]))
+    print(timestr)
+    return ping
+
 
