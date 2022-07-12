7e33c897310c7798c015f627b1eb1ee9205d636d
MatthieuDeroir <matthieu.deroir@epsi.fr>
2022-07-12 11:01:57 +0200

    ipcheck

--- ping.py
+++ ping.py
@@ -1,12 +1,8 @@
-import re
 import subprocess
-
 
 def ping(host):
     command = ['ping', '-c', '1', '-t', '1', host]
-    ping = subprocess.Popen(command, stdout=subprocess.PIPE)
-    timestr = re.compile("time=[0-9]+ms").findall(str(ping.communicate()[0]))
-    print(timestr)
-    return ping
-
+    # timestr = re.compile("time=[0-9]+ms").findall(str(ping.communicate()[0]))
+    # print(timestr)
+    return subprocess.call(command)
 
