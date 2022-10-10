2d227dc6490b5ea6ea0ed062097a188913458ad9
MatthieuDeroir <matthieu.deroir@epsi.fr>
2022-10-10 16:16:16 +0200

    telnet port test

--- telnet.py
+++ telnet.py
@@ -1,24 +1,27 @@
 import subprocess, os
-from config import port, backend_port, ip, frontend_port, timeout
+from config import port, backend_port, frontend_port, shebang
 
-
+ip = 'localhost'
+timeout = '5'
 
 
 def telnet():
     # telnet command to test different ports used by the server
     # grep -c return value greater than 0 if successful
-    bdd = "{echo -e '\02~NH02';}  | timeout --signal=9 " + timeout + " telnet " + ip + " " + str(port) + " | " \
-                                                                                            "(set -o pipefail && tee -a log.txt) | grep -c Connected"
-    frontend = "{echo -e '\02~NH02';}  | timeout --signal=9 " + timeout + " telnet " + ip + " " + str(frontend_port) + " | " \
-                                                                                                          "(set -o pipefail && tee -a log.txt) | grep -c Connected"
-    backend = "{echo -e '\02~NH02';}  | timeout --signal=9 " + timeout + " telnet " + ip + " " + str(backend_port) + " | " \
-                                                                                                        "(set -o pipefail && tee -a log.txt) | grep -c Connected"
+    bdd = shebang + " { echo -e '\02~NH02';}  | timeout --signal=9 " + timeout + " telnet " + ip + " " + str(port) + " | " \
+                                                                                            "(bash set +o pipefail && tee -a log.txt) | grep -c Connected"
+    frontend = shebang + " { echo -e '\02~NH02';}  | timeout --signal=9 " + timeout + " telnet " + ip + " " + str(frontend_port) + "| " \
+                                                                                                          "(bash set +o pipefail && tee -a log.txt) | grep -c Connected"
+    backend = shebang + " { echo -e '\02~NH02';}  | timeout --signal=9 " + timeout + " telnet " + ip + " " + str(backend_port) + "| " \
+                                                                                                        "(bash set +o pipefail && tee -a log.txt) | grep -c Connected"
 
-    rbdd = subprocess.Popen(bdd.split(), stdout=subprocess.PIPE, shell=True)
+    # return subprocess.call(bdd), subprocess.call(frontend), subprocess.call(backend)\
+    # replace os.system by subprocess.call on linux
+    rbdd = os.system(bdd)
 
     # reverse up and down value on linux
-    up_value = 0
-    down_value = 1
+    up_value = 1
+    down_value = 0
 
     if rbdd is up_value:
         print("Database return value : " + str(rbdd))
@@ -32,7 +35,7 @@ def telnet():
         print("Database return value : " + str(rbdd))
         print("Database port is not responding. MongoDB is probably DOWN !")
 
-    rfrontend = subprocess.Popen(frontend.split(), stdout=subprocess.PIPE, shell=True)
+    rfrontend = os.system(frontend)
 
     if rfrontend is up_value:
         print("Frontend return value : " + str(rfrontend))
@@ -46,8 +49,7 @@ def telnet():
         print("Frontend return value : " + str(rfrontend))
         print("Frontend port is not responding. Frontend is probably DOWN !")
 
-    rbackend = subprocess.Popen(backend.split(), stdout=subprocess.PIPE, shell=True)
-
+    rbackend = os.system(backend)
 
     if rbackend is up_value:
         print("Backend return value : " + str(rbackend))
@@ -61,7 +63,7 @@ def telnet():
         print("Backend return value : " + str(rbackend))
         print("Backend port is not responding. Backend is probably DOWN !")
 
-    error = rbdd or rfrontend or rbackend
+    error = not (rbdd or rfrontend or rbackend)
     print(error)
 
     # if any of the return value is 1 meaning there is an error somewhere it returns 1
