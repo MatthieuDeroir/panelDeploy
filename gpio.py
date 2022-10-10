3eab6391727273c58f0d2a37d5ae287948b81ce4
MatthieuDeroir <matthieu.deroir@epsi.fr>
2022-10-10 16:40:01 +0200

    telnet

--- gpio.py
+++ gpio.py
@@ -20,6 +20,7 @@ def update_input():
     door_2_status = GPIO.input(indexes['door_2'])
     screen_status = GPIO.input(indexes['screen'])
     button_status = GPIO.input(indexes['button'])
+    print("BUTTON =", button_status)
     return door_1_status, door_2_status, screen_status, button_status
 
 
