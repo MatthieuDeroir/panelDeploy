b188748d9ebbc7e3b4f5fa2a5ce6e7188e608d95
MatthieuDeroir <matthieu.deroir@epsi.fr>
2022-10-10 17:20:18 +0200

    p

--- telnet_osx.py
+++ telnet_osx.py
@@ -9,10 +9,10 @@ def telnet():
     # telnet command to test different ports used by the server
     # grep -c return value greater than 0 if successful
     bdd = "{ echo -e '\02~NH02';}  | timeout --signal=9 " + timeout + " telnet " + ip + " " + str(port) + " | " \
-                                                                                            "(bash set -o pipefail && tee -a log.txt) | grep -c Connected"
-    frontend = "{ echo -e '\02~NH02';}  | timeout --signal=9 " + timeout + " telnet " + ip + " " + str(frontend_port) + "| " \
+                                                                                            "(set pipefail && tee -a log.txt) | grep -c Connected"
+    frontend = "{ echo -e '\02~NH02';}  | timeout --signal=9 " + timeout + " telnet " + ip + " " + str(frontend_port) + " | " \
                                                                                                           "(set -o pipefail && tee -a log.txt) | grep -c Connected"
-    backend = "{ echo -e '\02~NH02';}  | timeout --signal=9 " + timeout + " telnet " + ip + " " + str(backend_port) + "| " \
+    backend = "{ echo -e '\02~NH02';}  | timeout --signal=9 " + timeout + " telnet " + ip + " " + str(backend_port) + " | " \
                                                                                                         "(set -o pipefail && tee -a log.txt) | grep -c Connected"
 
     # return subprocess.call(bdd), subprocess.call(frontend), subprocess.call(backend)\
